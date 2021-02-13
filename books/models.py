2ee0459b0873770768f05f9654534ca69a7cce4c
Fran Alonso <fraloal97@gmail.com>
2021-02-13 16:58:04 +0100

    Permissions per user added

--- books/models.py
+++ books/models.py
@@ -12,6 +12,12 @@ class Book(models.Model):
     cover = models.ImageField(upload_to='covers/', blank=True)
 
 
+    class Meta:
+        permissions = [
+            ('special_status', 'Can read all the books'),
+        ]
+
+
     def __str__(self):
         return self.title
 
